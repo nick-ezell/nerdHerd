9c56c7054c95967df8f7afb6699b6181c59d5fe7
Nick Ezell <nickolasezell@gmail.com>
2020-05-21 18:00:18 -0500

    Heroku deployment server setup.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,12 +8,20 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+}
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nerdHerder");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nerdHerder");
 
 // routes
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js"));
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
+  );
+}
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
