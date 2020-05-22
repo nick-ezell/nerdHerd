f83d3d4833105c0359f359c6ea75f2c9268f65e7
Nick Ezell <nickolasezell@gmail.com>
2020-05-22 15:03:57 -0500

    Foresaw a potential issue with Games model being require: true, changed to false in case of image link loss.

--- models/Games.js
+++ models/Games.js
@@ -3,9 +3,15 @@ const Schema = mongoose.Schema;
 
 const gamesSchema = new Schema({
   company: { type: String, required: true },
-  image: { type: String, required: true },
+  image: { type: String, required: false },
   title: { type: String, required: true },
   genre: { type: String, required: false },
+  communities: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Community",
+    },
+  ],
 });
 
 const Games = mongoose.model("Games", gamesSchema);
