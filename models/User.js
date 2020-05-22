ce513df5f8edd4e7304e3b7c80f9708547e6d1bf
Nick Ezell <nickolasezell@gmail.com>
2020-05-22 15:54:46 -0500

    Updated friends key in Users model to have an array of friend objects with a max of 25.

--- models/User.js
+++ models/User.js
@@ -8,7 +8,15 @@ const usersSchema = new Schema({
   dateJoined: { type: Date, default: Date.now, required: true },
   posts: [{ type: String, required: false }],
   communities: [{ type: String, required: false }],
-  squad: [{ type: String, required: false }],
+  friends: [
+    {
+      friendID: {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+      },
+      max: 25,
+    },
+  ],
   favGames: [{ type: String, required: false }],
   recentSearches: [{ type: String, required: false }],
   // Maybe add an image value for a user's user avatar picture?
