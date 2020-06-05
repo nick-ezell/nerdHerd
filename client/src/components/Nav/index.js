5362dfb536c4cada11f744acd4ba889a2bd16cdc
Nick Ezell <nickolasezell@gmail.com>
2020-06-04 19:19:33 -0500

    Intitialized folders for Game and GameList components, created .env file to handle sensitive api information.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Nav = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h2>nerdHerd</h2>
+      </div>
+
+      <div>
+        <a href="/login">Login</a>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
---
+++ client/src/utils/api/api.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+const queryURL = process.env.REACT_APP_API_URL;
+const key = process.env.REACT_APP_API_KEY;
